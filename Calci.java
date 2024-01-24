b073d346966bc171db5f8fba761f1aa2d5bc0a68
GAURAV12024 <2022bit022@sggs.ac.in>
2024-01-24 23:36:27 +0530

    multiplication function added to multiply to number

--- Calci.java
+++ Calci.java
@@ -1,7 +1,9 @@
 class Calci{
 
+    //num1 : first number
+    //num2: second number
+    
     //addition function to add two number 
-
     public static int add(num1,num2){
 
         return num1+num2;  // return sum of two number
@@ -10,8 +12,15 @@ class Calci{
     //substraction function to substract two number 
     public static int substraction(num1,num2){
         
-        return num1-num2;
+        return num1-num2; //return substaction of number
     }
+
+     //multiplication function to multiply two number 
+     public static int multiply(num1,num2){
+        
+        return num1*num2; //return product of two number
+    }
+    
 
    
 }
