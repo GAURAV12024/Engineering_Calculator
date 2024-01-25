9dce19bd4abe3553faaa21df719c16df1f893596
GAURAV12024 <2022bit022@sggs.ac.in>
2024-01-25 22:30:07 +0530

    modify the funnction return type and changes function from static to non static

--- Calci.java
+++ Calci.java
@@ -5,56 +5,74 @@ class Calci{
     //num2: second number
 
     //addition function to add two number 
-    public static int add(num1,num2){
+    public int add(int num1,int num2){
 
         return num1+num2;  // return sum of two number
     }
 
     //substraction function to substract two number 
-    public static int substraction(num1,num2){
+    public int substraction(int num1,int num2){
         
         return num1-num2; //return substaction of number
     }
 
      //multiplication function to multiply two number 
-     public static int multiply(num1,num2){
+     public double multiply(double num1,double num2){
         
         return num1*num2; //return product of two number
     }
 
      //Cube function to get cube of a number 
-     public static int Cube(num1){
+     public double Cube(double num1){
         
         return num1*num1*num1; //return product of two number
     }
 
-
-
-
     //Square function to get square of number 
-    public static int square(num1){
+    public double square(double num1){
         
         return num1*num1; //return square of a number
     }
 
     //SquareRoot function to get squareRoot of a number 
-    public static int squareRoot(num1){
+    public double squareRoot(double num1){
         
         return Math.sqrt(num1); //return squareRoot of a number
     }
 
     //division function to divide two number 
-    public static int divide(num1,num2){
+    public double divide(double num1,double num2){
         
         return num1/num2; //return division of two number
     }
     //function to get remainder when we divide two number 
-    public static int remainder(num1,num2){
+    public int remainder(int num1,int num2){
         
         return num1 % num2; //return remainder when we divide num1 by num2
 
     }
 
+
+    public static void main(String args[]){
+
+        int a=5;
+
+        int b=4;
+
+        Calci c=new Calci();
+
+        System.out.println(c.add(a,b));
+        System.out.println(c.substraction(a,b));
+        System.out.println(c.multiply(a,b));
+        System.out.println(c.divide(a,b));
+        System.out.println(c.Cube(a));
+        System.out.println(c.square(a));
+        System.out.println(c.remainder(a,b));
+        System.out.println(c.squareRoot(a));
+
+
+
+    }
 
     
 
